f63e787cf41aab485fe3770da2f817a42c3df14f
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-04-07 09:39:42 +0700

    Add project files.

---
+++ easd_1_159_D/easd_1_159_D.cpp
@@ -0,0 +1,13 @@
+//1. karena hanya algoritma yang dapat menyelesaikan dan algoritma juga mempunyai langkah-langkah yang sistematis dan tersetruktur.
+//2. setruktur data linier dan struktur data non-linier.
+//3. Ukuran input, Struktur data, Kompleksitas algoritma, Bahasa pemograman, Platform hardware.
+//4. Inserttion sort karena salah satu algoritma pengurutan sederhana yang berguna untuk mengurutkan data dalam jumlah kecil hingga sedang dengan efisien.
+//5.	Quadratic : Bobble sort, Selection sort, Insertion sort.
+//		Loglinear : Quick sort
+//6. 
+
+#include <iostream>
+using namespace std;
+
+
+
